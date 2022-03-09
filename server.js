d8c9005e5866f20a1cdeb4fb376d86711fbf327c
Ian Pous <pousig@g.crookcountyschools.org>
2022-03-08 17:48:36 -0800

    Lab 8 and 9

--- server.js
+++ server.js
@@ -1,54 +1,61 @@
-'use strict'
+'use strict';
 
-// require in our libraries
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-//bring in our weather dummy data
-const weather = require('./data/weather.json');
-const { response } = require('express');
-
-//App to use...
+const { request, response } = require('express');
+//const weather = require('./data/weather.json');
 const app = express();
 app.use(cors());
-
+const weather = require('./modules/weather.js');
+const axios = require('axios');
 //set the port variable
 const PORT = process.env.PORT || 3002;
 
-//testing the home route to make sure the server is running correctly
 app.get('/', (request, response) => {
     response.send('hello from home route')
 });
+app.get('./weather', weatherHandler);
+async function weatherHandler(request, response) {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    console.log('lat', 'lon', lat, lon);
+     await weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch(error => {
+        console.error(error);
+        response.status(500).send('Sorry something went wrong');
+    })
+}
 
-//Create an API endpoint of `/weather` that processes a `GET` request that contains `lat`, `lon`, and `searchQuery` information.
-app.get('/weather', (request, response) => {
+app.get('/weather', async (request, response) => {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
 
-    //Use the .find() method to discover which city the `lat`, `lon` and `searchQuery` information belong to. 
-    const city = weather.find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase()); 
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
 
-    try{
-        //Using each data point from the static data of the city that the user searched, create an array of `Forecast` objects, one for each day. 
-        const weatherArray = city.data.map(day => new Forecast(day)); 
-
-        //Send the full array back to the client who requested data from the `weather` endpoint.
+    try {
+        const weatherData = await axios.get(url);
+        console.log(weatherData.data.data)
+        const weatherArray = weatherData.data.data.map(day => new Forecast(day));
+        //const weatherData = city.data.map(day => new Forecast(day));
         response.status(200).send(weatherArray);
 
-    } catch(error) {
-//If the user did not search for one of the three cities that we have information about (Seattle, Paris, or Amman), return an error.
-    console.log(error);
-    response.status(500).send('city not found')
+
+    } catch ( error ) {
+        console.log(error);
+        response.status(500).send('city not found');
+        //response.send('city not found');
     }
 
-   })
+})
+function Forecast(day) {
+    this.day = day.valid_date
+    this.description = day.weather.description
+}
 
-   //Create a class for `Foreclass`, that has properties of `date` and `description`.
-   function Forecast(day) {
-       this.day = day.valid_date
-       this.description = day.weather.description
-   }
+app.use('*', (request, response) => response.status(404).send('that end point does not exist'));
 
-//tells the express app which port to listen on
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
